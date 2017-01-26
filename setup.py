3b3d83d679fdcb77bb9aca3b4b2e2d499b544787
Alejandro <alemail@gmail.com>
2017-01-27 00:46:25 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name='tooansi',
     version='1.0',
+    author ="Alejandro Murciano",
+    author_email='alejnd@gmail.com',
+    url='https://github.com/alejnd/tooansi',
     long_description=__doc__,
     #packages=['tooansi'],
+    #package_dir={'flask': 'flask'},
     packages=find_packages(),
     #include_package_data=True,
+    scripts = ["run.py","config.py","setup.py","test_tooansi.py","png2ansi.py"],
     zip_safe=False,
     install_requires=['Flask']
 )
